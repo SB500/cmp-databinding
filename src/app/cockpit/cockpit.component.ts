cb87680f3bc96d1298c004cd5fbdaca021d1b5dc
sb500 <bankysj@gmail.com>
2019-12-01 06:58:58 -0600

    first commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+    selector: 'app-cockpit',
+    templateUrl: './cockpit.component.html'
+})
+
+export class CockpitComponent implements OnInit {
+    @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+    @Output() bluePrintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+    newServerName = '';
+    newServerContent = '';
+
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    onAddServer() {
+        this.serverCreated.emit({
+            serverName: this.newServerName,
+            serverContent: this.newServerContent
+        });
+    }
+    onAddBluePrint() {
+        this.bluePrintCreated.emit({
+            serverName: this.newServerName,
+            serverContent: this.newServerContent
+        });
+    }
+}
+
